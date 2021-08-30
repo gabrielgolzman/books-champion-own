0ac0a29488fc9b651b19eaa8449be26cf2deef6d
gabrielgolzman <gabrielgolzman@gmail.com>
2021-08-30 17:39:18 -0300

    Class 3 : first setup and mobile clean up

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -4,12 +4,17 @@ import ReadDate from './ReadDate';
 import BookCard from './BookCard';
 
 const BookItem = ({ title, author, dateRead, pageCount }) => {
+   const changeTitle = () => {
+      console.log('Clicked!!');
+   };
+
    return (
       <BookCard>
          <h2>{title}</h2>
          <h3>{author}</h3>
          <ReadDate date={dateRead} />
          <p>{pageCount} páginas</p>
+         <button onClick={changeTitle}>Cambiar titulo</button>
       </BookCard>
    );
 };
