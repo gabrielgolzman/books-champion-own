4f9c2790327043746c179171e17acd495d5b8f39
gabrielgolzman <gabrielgolzman@gmail.com>
2021-08-30 17:01:18 -0300

    Class 2 : finished

---
+++ src/components/ReadDate.js
@@ -0,0 +1,17 @@
+import './ReadDate.css';
+
+const ReadDate = ({ date }) => {
+   const year = date.getFullYear();
+   const month = date.toLocaleString('es-AR', { month: 'long' });
+   const day = date.toLocaleString('es-AR', { day: '2-digit' });
+
+   return (
+      <div className="date-container">
+         <div>{year}</div>
+         <div>{month}</div>
+         <div>{day}</div>
+      </div>
+   );
+};
+
+export default ReadDate;
