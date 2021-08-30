66842a628ef1f009952e159d0784136318c916db
gabrielgolzman <gabrielgolzman@gmail.com>
2021-08-30 13:16:36 -0300

    Reformat of the base class

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import Books from './components/Books/Books';
-import NewBook from './components/NewBook/NewBook';
+import BookItem from './components/BookItem';
 
 const App = () => {
-   const books = [
-      {
-         title: '100 años de soledad',
-         pageCount: 410,
-         date: new Date('2021-07-12'),
-      },
-      {
-         title: 'Todos los fuegos el fuego',
-         pageCount: 197,
-         date: new Date('2021-06-11'),
-      },
-      {
-         title: 'Asesinato en el Orient Express',
-         pageCount: 256,
-         date: new Date('2021-05-09'),
-      },
-      {
-         title: 'Las dos torres',
-         pageCount: 352,
-         date: new Date('2021-03-22'),
-      },
-   ];
-
-   const addedBookHandler = (bookData) => {
-      console.log('In App.js');
-      console.log(bookData);
-   };
-
    return (
       <div>
          <h2>Books Champion App</h2>
-         <NewBook onBookAdded={addedBookHandler} />
-         <Books books={books} />
+         <p>¡Quiero leer libros!</p>
+         <BookItem></BookItem>
       </div>
    );
 };
---
+++ src/components/BookItem.js
@@ -0,0 +1,14 @@
+import './BookItem.css';
+
+const BookItem = () => {
+   return (
+      <div className="book-item">
+         <h1>Título</h1>
+         <h2>Autor</h2>
+         <h3>Fecha de lectura</h3>
+         <h4>Páginas</h4>
+      </div>
+   );
+};
+
+export default BookItem;
